129f37e090f4aeb2913203f2e32e412739583acc
pjmantoss <joelptoss@gmail.com>
2020-04-16 14:58:39 +0100

    update script.js

--- script.js
+++ script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const squares = document.querySelectorAll('.grid div');
     const result = document.querySelector('#result');
     const displayResult = document.querySelector('.current-player');
+
+    let currentPlayer = 1;
+    let len = squares.length;
+
+    for (let i = 0; i < len; i++)
+
+    (index = () => {
+        //add an onClick to each square in the grid
+        squares[i].onclick = function(){
+            //if the square below your current square is taken, you can go on top of it
+            if(squares[index + 7].classList.contains('taken')){}
+        }
+    })
 })
